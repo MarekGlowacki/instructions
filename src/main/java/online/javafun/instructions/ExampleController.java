18fe65be70d8f4cefa4e82857cb3caf267d23775
User <user@its.me>
2023-01-24 09:51:37 +0100

    condition instructions in thymeleaf

--- src/main/java/online/javafun/instructions/ExampleController.java
+++ src/main/java/online/javafun/instructions/ExampleController.java
@@ -1,2 +1,16 @@
-package online.javafun.instructions;public class ExampleController {
+package online.javafun.instructions;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ExampleController {
+
+    @GetMapping("/")
+    String example(Model model) {
+        model.addAttribute("person", new Person("Łukasz", "Górski", 18));
+        model.addAttribute("lang", "es");
+        return "example";
+    }
 }
